71dac925c03c2448528d44bb6cc9b3f8b6a51612
Arjin Joshi <thearjinjoshi1@gmail.com>
2023-02-15 00:34:57 +0545

    Day1 [COMPLETED]

---
+++ day1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a = 2;
+    int b = 5;
+
+    cout << !(a == 2) << endl;
+
+    if (a <= 3 && b >= 3)
+    {
+        cout << "LImit Reached" << endl;
+    }
+
+    cout << (5.0) / 3 << endl;
+    cout << (5) / 3.0;
+
+    char ch = 99;
+    cout << ch << endl;
+
+    double d = 5.7;
+    int x = (int)d + 2;
+    cout << "value of x:" << x << endl;
+
+    char ab = 97;
+    cout << ab << endl;
+
+    int num = 'b';
+    cout << num << endl;
+
+    long e = 5;
+    cout << sizeof(e) << endl;
+
+    cout << 2;
+    cout << endl;
+    cout << "Arjin\n"
+         << endl;
+    cout << '\n';
+}
